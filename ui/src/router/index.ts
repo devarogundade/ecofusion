0ddb6a3b0725a12a576bda0118be3e4057b0006c
devarogundade <devarogundade@gmail.com>
2024-12-18 17:03:15 +0100

    added scroll bev

--- ui/src/router/index.ts
+++ ui/src/router/index.ts
@@ -11,6 +11,13 @@ import DonationView from "@/views/DonationView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
   routes: [
     {
       path: "/",
